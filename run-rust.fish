00a48aeaf5aa8c42c3d5340c5e4137e4ea004d6b
LianSheng197 <ci.i.cxcvii@gmail.com>
2022-05-01 19:52:16 +0800

    [Tool] Add clear segmentation

--- run-rust.fish
+++ run-rust.fish
@@ -3,9 +3,17 @@
 function rust --description "Fast complie and run for leetcode | rust <target.rs>"
     set target $argv[1]
 
+    set_color green
+    echo -e "\n------------[ Compiling ]------------\n"
+    set_color normal
+
     rustc $target --out-dir rustbin
 
     if test $status -eq 0
+        set_color green
+        echo -e "\n------------[ Running ]------------\n"
+        set_color normal
+
         set filename (echo $target | sed 's/\.[^.]*$//')
         eval rustbin/$filename
     end
